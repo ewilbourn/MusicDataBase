1244e1b8100763f3473fbdc94bb5ac4c535f95a0
Emily Wilbourn <ewilbourn2111@gmail.com>
2021-12-14 22:12:50 -0500

    Added a filter for difficulty level; also, I noticed that the dropdown filters filter out all data contained in database, not just what is shown in the dataTable; so if a couple of pieces were hidden from the view bc they have null opus or number values, their year/difficulty levels would still be options in the dropdown filters

--- MusicWebsite/Controllers/MusicPiecesController.cs
+++ MusicWebsite/Controllers/MusicPiecesController.cs
@@ -21,11 +21,11 @@ namespace MusicWebsite.Controllers
         public ActionResult Index()
         {
             var musicPieces = dal.DbContext.MusicPieces.Include(m => m.Composer).Include(m => m.OpusNamingConvention);
-            setFilters(0, 0);
+            setFilters(0, 0, 0);
             return View(musicPieces.ToList());
         }
 
-        private void setFilters(int composerDefault = 0, int yearDefault = 0)
+        private void setFilters(int composerDefault = 0, int yearDefault = 0, int levelDefault = 0)
         {
             List<ListItem> compItems = new List<ListItem>();
             var compList = dal.DbContext.Composers.ToList();
@@ -35,11 +35,18 @@ namespace MusicWebsite.Controllers
             ViewBag.ComposerFilter = compSelectList;
 
             List<ListItem> yearsItems = new List<ListItem>();
-            var yearsList = dal.DbContext.MusicPieces.Where(w=> w.Year != null).Select(s=> new { MusicYear = (int)s.Year }).Distinct().ToList();
+            var yearsList = dal.DbContext.MusicPieces.Where(w => w.Year != null).Select(s => new { MusicYear = (int)s.Year }).Distinct().ToList();
             yearsItems = yearsList.Select(s => new ListItem(s.MusicYear.ToString(), s.MusicYear.ToString())).ToList();
             yearsItems.Insert(0, new ListItem("All", ""));
             SelectList yearsSelectList = new SelectList(yearsItems, "Value", "Text", yearDefault > 0 ? yearDefault.ToString() : null);
             ViewBag.YearFilter = yearsSelectList;
+
+            List<ListItem> diffItems = new List<ListItem>();
+            var diffList = dal.DbContext.MusicPieces.Where(w => w.Difficulty != null).Select(s => new { MusicDifficulty = (int)s.Difficulty }).Distinct().ToList();
+            diffItems = diffList.Select(s => new ListItem(s.MusicDifficulty.ToString(), s.MusicDifficulty.ToString())).ToList();
+            diffItems.Insert(0, new ListItem("All", ""));
+            SelectList difficultySelectList = new SelectList(diffItems, "Value", "Text", levelDefault > 0 ? levelDefault.ToString() : null);
+            ViewBag.DifficultyFilter = difficultySelectList;
         }
 
         /// <summary>
@@ -55,6 +62,7 @@ namespace MusicWebsite.Controllers
                                            where 
                                                 (p.ComposerID == 0 || d.ComposerID == p.ComposerID) &&
                                                 (p.Year == 0 || p.Year == d.Year) &&
+                                                (p.Difficulty == 0 || p.Difficulty == d.Difficulty) &&
                                                 (p.Search.Value == null ||
                                                     (
                                                     (d.FullName.Contains(p.Search.Value)) ||
@@ -126,6 +134,17 @@ namespace MusicWebsite.Controllers
                 return HttpNotFound();
             }
             return View(musicPiece);
+        }
+
+        //GET: Hover agent
+        public ActionResult HoverAgent(int? id)
+        {
+            Composer composerDetails = dal.DbContext.Composers.Find(id);
+            if (composerDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(composerDetails);
         }
 
         // GET: MusicPieces/Create
